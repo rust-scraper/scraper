6c2ec01d64cc814dcf9cb95c6d67f507fe3bc5e3
Curtis McEnroe <programble@gmail.com>
2016-01-01 17:11:47 -0500

    Add dependencies and warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
-#[test]
-fn it_works() {
-}
+//! # Scraper
+//!
+//! Dumpster diving for that precious web content.
+
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results,
+    variant_size_differences
+)]
