5dcf76035358a4d55cdd354a2cf981e567af3254
CFV <vescovocarlofederico@gmail.com>
2021-11-10 22:34:07 +0100

    Fix warning by formatting error string properly

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
-        Err(f) => panic!(f.to_string()),
+        Err(f) => panic!("{}", f.to_string()),
     };
     if matches.opt_present("h") {
         print!(
