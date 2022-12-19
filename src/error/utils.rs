bfae7055253449a42d395639702e52da2d3323dc
Kiwifuit <mahkiwi123@gmail.com>
2022-12-19 08:16:44 +0800

    Added helper function to render single-character tokens

--- src/error/utils.rs
+++ src/error/utils.rs
@@ -32,29 +32,33 @@ pub(crate) fn render_token<'a>(token: &Token<'a>) -> String {
         Token::BadString(string) => format!("<Bad String {:?}>", string),
         Token::BadUrl(url) => format!("<Bad URL {:?}>", url),
         // Single-character token
-        sc_token => String::from(match sc_token {
-            Token::Colon => ":",
-            Token::Semicolon => ";",
-            Token::Comma => ",",
-            Token::IncludeMatch => "~=",
-            Token::DashMatch => "|=",
-            Token::PrefixMatch => "^=",
-            Token::SuffixMatch => "$=",
-            Token::SubstringMatch => "*=",
-            Token::CDO => "<!--",
-            Token::CDC => "-->",
-            Token::ParenthesisBlock => "<(",
-            Token::SquareBracketBlock => "<[",
-            Token::CurlyBracketBlock => "<{",
-            Token::CloseParenthesis => "<)",
-            Token::CloseSquareBracket => "<]",
-            Token::CloseCurlyBracket => "<}",
-            other => panic!(
-                "Token {:?} is not supposed to match as a single-character token!",
-                other
-            ),
-        }),
+        sc_token => render_single_char_token(&sc_token),
     }
+}
+
+fn render_single_char_token(token: &Token) -> String {
+    String::from(match token {
+        Token::Colon => ":",
+        Token::Semicolon => ";",
+        Token::Comma => ",",
+        Token::IncludeMatch => "~=",
+        Token::DashMatch => "|=",
+        Token::PrefixMatch => "^=",
+        Token::SuffixMatch => "$=",
+        Token::SubstringMatch => "*=",
+        Token::CDO => "<!--",
+        Token::CDC => "-->",
+        Token::ParenthesisBlock => "<(",
+        Token::SquareBracketBlock => "<[",
+        Token::CurlyBracketBlock => "<{",
+        Token::CloseParenthesis => "<)",
+        Token::CloseSquareBracket => "<]",
+        Token::CloseCurlyBracket => "<}",
+        other => panic!(
+            "Token {:?} is not supposed to match as a single-character token!",
+            other
+        ),
+    })
 }
 
 fn render_number(signed: bool, num: f32, token: &Token) -> String {
