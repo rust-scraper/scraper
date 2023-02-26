aa479ea170b7f2b463ccba85de0c802318d19abe
Jeff Mendez <jeff@a11ywatch.com>
2023-02-26 16:01:57 -0500

    perf(element): add one sweep element creation (#109)
    * perf(element): add one sweep element creation
    
    * perf(element): add fast value dref handling
    
    * chore(minor): add none usage default id
    
    * chore(minor): add if else value deref
    
    * chore(minor): revert a.value deref split
    
    * chore(minor): add match local name deref

--- src/node.rs
+++ src/node.rs
@@ -8,7 +8,6 @@ use std::ops::Deref;
 
 use html5ever::tendril::StrTendril;
 use html5ever::{Attribute, LocalName, QualName};
-
 use selectors::attr::CaseSensitivity;
 
 /// An HTML node.
@@ -238,25 +237,26 @@ pub struct Element {
 
 impl Element {
     #[doc(hidden)]
-    pub fn new(name: QualName, attrs: Vec<Attribute>) -> Self {
-        let id = attrs
-            .iter()
-            .find(|a| a.name.local.deref() == "id")
-            .map(|a| LocalName::from(a.value.deref()));
+    pub fn new(name: QualName, attributes: Vec<Attribute>) -> Self {
+        let mut classes: HashSet<LocalName> = HashSet::new();
+        let mut attrs = Attributes::with_capacity(attributes.len());
+        let mut id: Option<LocalName> = None;
 
-        let classes: HashSet<LocalName> = attrs
-            .iter()
-            .find(|a| a.name.local.deref() == "class")
-            .map_or_else(HashSet::new, |a| {
-                a.value
-                    .deref()
-                    .split_whitespace()
-                    .map(LocalName::from)
-                    .collect()
-            });
+        for a in attributes {
+            match a.name.local.deref() {
+                "id" => {
+                    id = Some(LocalName::from(a.value.deref()));
+                }
+                "class" => {
+                    classes.extend(a.value.deref().split_whitespace().map(LocalName::from));
+                }
+                _ => (),
+            };
+            attrs.insert(a.name, a.value);
+        }
 
         Element {
-            attrs: attrs.into_iter().map(|a| (a.name, a.value)).collect(),
+            attrs,
             name,
             id,
             classes,
