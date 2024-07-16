b3570f31e1e3346de9a13ee9fa054101e82bf429
Carlo Federico Vescovo <26970569+cfvescovo@users.noreply.github.com>
2024-07-16 13:14:32 +0200

    `is` and `has` support

--- src/test.rs
+++ src/test.rs
@@ -20,3 +20,27 @@ fn tag_with_newline() {
         Some("https://github.com/causal-agent/scraper")
     );
 }
+
+#[test]
+fn has_selector() {
+    let document = Html::parse_fragment(
+        r#"
+        <div>
+            <div id="foo">
+                Hi There!
+            </div>
+        </div>
+        <ul>
+            <li>first</li>
+            <li>second</li>
+            <li>third</li>
+        </ul>
+        "#,
+    );
+
+    let selector = Selector::parse("div:has(div#foo) + ul > li:nth-child(2)").unwrap();
+
+    let mut iter = document.select(&selector);
+    let li = iter.next().unwrap();
+    assert_eq!(li.inner_html(), "second");
+}
