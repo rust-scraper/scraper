3dd3c2372c2dc698ccb7a997b2d736ddec3e7eba
LoZack19 <giozaccaria02@gmail.com>
2024-08-30 11:53:01 +0200

    fix formatting with rustfmt

--- scraper_proc_macros/src/lib.rs
+++ scraper_proc_macros/src/lib.rs
@@ -5,14 +5,17 @@ use syn::{parse_macro_input, LitStr};
 #[proc_macro]
 pub fn selector(input: TokenStream) -> TokenStream {
     let selector = parse_macro_input!(input as LitStr);
-    
+
     match scraper::Selector::parse(&selector.value()) {
         Ok(_) => quote!(
             ::scraper::Selector::parse(#selector).unwrap()
-        ).into(),
+        )
+        .into(),
         Err(e) => syn::Error::new(
             proc_macro2::Span::call_site(),
-            format!("Failed to parse CSS selector: {}", e)
-        ).to_compile_error().into(),
+            format!("Failed to parse CSS selector: {}", e),
+        )
+        .to_compile_error()
+        .into(),
     }
 }
